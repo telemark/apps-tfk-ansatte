b85bc14204f18eb7184e45333298e785c0849067
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-04-07 13:00:22 +0200

    Fixes search, adds ansatt route

--- js/app.js
+++ js/app.js
@@ -2,27 +2,21 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
-  this.resource('avdelinger');
+  this.route('avdelinger');
   this.resource('avdeling', {'path':'/avdelinger/:avdeling_id'}, function(){
-    this.resource('kart');
+    this.route('kart');
   });
-  this.resource('search', {'path':'/search/:query'})
+  this.route('search', {'path':'/search/:query'})
   this.route('personvern');
+  this.route('ansatt', {'path':'/ansatt/:ansatt_id'});
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-  },
+App.ApplicationController = Ember.Controller.extend({
+  search:'',
   actions:{
-    doSearch: function(){
-      var param = App.query;
-      return jQuery.ajax({
-        url: 'http://ws.t-fk.no/?resource=persons&search=fullname&string=' + param + '&format=json',
-        dataType: 'jsonp',
-        type: 'GET'
-      }).then(function(json){
-        return json.results;
-      });
+    doSearch:function(){
+      var q = this.get('search');
+      this.transitionToRoute('search', q);
     }
   }
 });
@@ -43,6 +37,18 @@ App.AvdelingRoute = Ember.Route.extend({
   model: function(params) {
     return jQuery.ajax({
       url: 'http://ws.t-fk.no/?resource=departments&search=departmentid&string=' + params.avdeling_id + '&format=json',
+      dataType: 'jsonp',
+      type: 'GET'
+    }).then(function(json){
+      return json.results[0];
+    });
+  }
+});
+
+App.AnsattRoute = Ember.Route.extend({
+  model: function(params) {
+    return jQuery.ajax({
+      url: 'http://ws.t-fk.no/?resource=persons&search=personid&string=' + params.ansatt_id + '&format=json',
       dataType: 'jsonp',
       type: 'GET'
     }).then(function(json){
