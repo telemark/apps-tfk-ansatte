c34cf08e218ec2e4e34b8b31ce19583efb86eb34
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-04-08 13:48:46 +0200

    Implements ansatte and kart as subroutes to avdeling

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
   this.route('avdelinger');
   this.resource('avdeling', {'path':'/avdelinger/:avdeling_id'}, function(){
+    this.route('ansatte');
     this.route('kart');
   });
   this.route('search', {'path':'/search/:query'})
@@ -33,28 +33,34 @@ App.SearchRoute = Ember.Route.extend({
   }
 })
 
-
 App.AvdelingRoute = Ember.Route.extend({
   model: function (params) {
-
-    var avdelingCall = jQuery.ajax({
+    return jQuery.ajax({
         url: 'http://ws.t-fk.no/?resource=departments&search=departmentid&string=' + params.avdeling_id + '&format=json',
         dataType: 'jsonp',
         type: 'GET'
-      }),
-        ansatteCall = jQuery.ajax({
-          url: 'http://ws.t-fk.no/?resource=persons&search=departmentid&string=' + params.avdeling_id + '&format=json',
-          dataType: 'jsonp',
-          type: 'GET'
-        });
-
-    return Ember.RSVP.Promise.all([avdelingCall,ansatteCall]).then(function(arr){
-      var avdeling = arr[0].results[0],
-          ansatte = arr[1].results;
-
-      return {avdeling:avdeling, ansatte:ansatte};
+      }).then(function(json){
+        return json.results[0];
     });
+  }
+});
 
+App.AvdelingAnsatteRoute = Ember.Route.extend({
+  model: function(){
+    var avdeling_id = this.modelFor('avdeling').departmentId;
+    return jQuery.ajax({
+      url: 'http://ws.t-fk.no/?resource=persons&search=departmentid&string=' + avdeling_id + '&format=json',
+      dataType: 'jsonp',
+      type: 'GET'
+    }).then(function(json){
+      return json.results;
+    });
+  }
+});
+
+App.AvdelingKartRoute = Ember.Route.extend({
+  model: function(){
+    return this.modelFor('avdeling');
   }
 });
 
