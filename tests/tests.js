6e78a329871c8fe738e0257dd725440e1bb5ebb6
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-04-10 10:30:57 +0200

    Adds some tests

--- tests/tests.js
+++ tests/tests.js
@@ -25,7 +25,21 @@ test("/", function() {
 
   // helper waiting the application is idle before running the callback
   andThen(function() {
-    equal(find("h2").text(), "Welcome to Ember.js", "Application header is rendered");
+    equal(find("h1").text(), "Finn ansatte", "Page title is correct");
+    equal(find("h3").text(), "Telemark fylkeskommune: ansatte", "Application header is rendered");
     equal(find("li").length, 3, "There are three items in the list");
+  });
+});
+
+test("/personvern", function() {
+  // async helper telling the application to go to the '/' route
+  visit("/personvern");
+
+  // helper waiting the application is idle before running the callback
+  andThen(function() {
+    equal(find("h1").text(), "Personvernerklæring", "Page title is correct");
+    equal(find("h3").first().text(), "Telemark fylkeskommune: ansatte", "Application header is rendered");
+    equal(find("h3").last().text(), "Les mer om cookies", "Application header is rendered");
+    equal(find("h2").text(), "Bruk av Google Analytics", "Sub title is correct");
   });
 });
